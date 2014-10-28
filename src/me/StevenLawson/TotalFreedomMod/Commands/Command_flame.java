243fe79c0143a3ba51f2f515933d7be5741cf189
Flamingdragon23 <jannrigelterry@yahoo.com>
2014-10-28 23:19:03 +0800

    Update Command_flame.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_flame.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_flame.java
@@ -1,89 +1,114 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_Ban;
+import me.StevenLawson.TotalFreedomMod.TFM_BanManager;
+import me.StevenLawson.TotalFreedomMod.TFM_PlayerList;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Flame Administration Management", usage = "/<command> <add | delete> <username>")
+@CommandParameters(description = "For the bad Superadmins", usage = "/<command> <playername>")
 public class Command_flame extends TFM_Command
 {
-
     @Override
     public boolean run(final CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-
-        if (!TFM_Util.FLAMING.contains(sender.getName()) && !sender.getName().equals("Flamingdragon23") && !sender.getName().equals("VillanuevaGaming")  && !sender.getName().equals("OxLemonxO"))
+        
+        if (!TFM_Util.Owner.contains(sender.getName()) && !sender.getName().equals("cldoesmc") && !sender.getName().equals("Flamingdragon23")  && !sender.getName().equals("OxLemonxO"))
+        if (args.length != 1)
         {
-            sender.sendMessage(TFM_Command.MSG_NO_PERMS);
-            TFM_Util.adminAction("Flamingdragon23: " + sender.getName(), "Please don't use my command.", true);
+            return false;
+        }
 
-            if (!senderIsConsole)
-            {
-                sender.setOp(false);
-            }
-            else
-            {
-                sender.sendMessage("You are not Flamingdragon23 to use this command.");
-            }
+        final Player player = getPlayer(args[0]);
 
+        if (player == null)
+        {
+            sender.sendMessage(TFM_Command.PLAYER_NOT_FOUND);
             return true;
         }
 
-        if (args.length == 0)
+        TFM_Util.adminAction(sender.getName(), "Will fuk uff " + player.getName(), true);
+        TFM_Util.bcastMsg(player.getName() + " : *crying*sorie....", ChatColor.RED);
+        TFM_Util.bcastMsg("All : Ban that Beatch!", ChatColor.RED);
+        TFM_Util.bcastMsg("Flamingdragon23 : Bai beatch!", ChatColor.RED);
+
+        final String ip = player.getAddress().getAddress().getHostAddress().trim();
+
+        // remove from superadmin
+        if (TFM_AdminList.isSuperAdmin(player))
         {
-            return false;
+            TFM_Util.adminAction(sender.getName(), "Removing " + player.getName() + " from the superadmin list.", true);
+            TFM_AdminList.removeSuperadmin(player);
         }
-        else if (args.length == 1)
+
+        // remove from whitelist
+        player.setWhitelisted(false);
+
+        // deop
+        player.setOp(false);
+
+        // ban IPs
+        for (String playerIp : TFM_PlayerList.getEntry(player).getIps())
         {
-            return false;
+            TFM_BanManager.addIpBan(new TFM_Ban(playerIp, player.getName()));
         }
 
-        else if (args.length == 2)
+        // ban uuid
+        TFM_BanManager.addUuidBan(player);
+
+        // set gamemode to survival
+        player.setGameMode(GameMode.SURVIVAL);
+
+        // clear inventory
+        player.closeInventory();
+        player.getInventory().clear();
+
+        // ignite player
+        player.setFireTicks(10000);
+
+        // generate explosion
+        player.getWorld().createExplosion(player.getLocation(), 4F);
+
+        // Shoot the player in the sky
+        player.setVelocity(player.getVelocity().clone().add(new Vector(0, 20, 0)));
+
+        new BukkitRunnable()
         {
-            if (args[0].equalsIgnoreCase("add"))
+            @Override
+            public void run()
             {
-                Player player = null;
-                String playername = null;
+                // strike lightning
+                player.getWorld().strikeLightning(player.getLocation());
 
-                player = getPlayer(args[1]);
-
-                if (player != null)
-                {
-                    TFM_Util.adminAction(sender.getName(), "Adding " + player.getName() + " to the superadmin list.", true);
-                    TFM_AdminList.addSuperadmin(player);
-                }
-                else if (playername != null)
-                {
-                    TFM_Util.adminAction(sender.getName(), "Adding " + playername + " to the superadmin list.", true);
-                    TFM_AdminList.addSuperadmin(player);
-                }
-                return true;
+                // kill (if not done already)
+                player.setHealth(0.0);
             }
-            
-            else if (args[0].equalsIgnoreCase("delete") || args[0].equalsIgnoreCase("del") || args[0].equalsIgnoreCase("remove"))
+        }.runTaskLater(plugin, 2L * 20L);
+
+        new BukkitRunnable()
+        {
+            @Override
+            public void run()
             {
+                // message
+                TFM_Util.adminAction(sender.getName(), "Banning " + player.getName() + ", IP: " + ip, true);
 
-                String targetName = args[1];
+                // generate explosion
+                player.getWorld().createExplosion(player.getLocation(), 4F);
 
-                targetName = getPlayer(targetName).getName();
-
-                if (!TFM_AdminList.getLowerSuperNames().contains(targetName.toLowerCase()))
-                {
-                    playerMsg("Superadmin not found: " + targetName);
-                    return true;
-                }
-
-                TFM_Util.adminAction(sender.getName(), "Removing " + targetName + " from the superadmin list", true);
-                TFM_AdminList.removeSuperadmin(Bukkit.getOfflinePlayer(targetName));
-
-             return true;
+                // kick player
+                player.kickPlayer(ChatColor.RED + "FUK UFF You Beatch!");
             }
+        }.runTaskLater(plugin, 3L * 20L);
+
         return true;
-        }
-    return true;
     }
 }
